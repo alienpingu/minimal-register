a78bb3cbe4f622946df65dfdee2cf1c1e36a6947
alienpingu <samuele.bertocco@gmail.com>
2021-03-09 18:22:51 +0100

    general deploy fix

--- script.js
+++ script.js
@@ -75,22 +75,42 @@ validateField = (c, v) => {
 
 
 
-		if (result === 0 && v !== '') {
+		if (result === 0 && v !== '' && v !== undefined) {
 
 			switch(c) {
 				case 'email':
 
-					if (document.querySelector('#emailRegister').classList.contains('invalid')) {
-						document.querySelector('#emailRegister').classList.remove('invalid');
-					}
+					let emailValidity = validateEmail(v);
 
-					if (!document.querySelector('#emailRegister').classList.contains('valid')) {
-						document.querySelector('#emailRegister').classList.add('valid');
-					}
+					if (emailValidity) {
+						if (!document.querySelector('#emailRegister').classList.contains('valid')) {
+							document.querySelector('#emailRegister').classList.add('valid');
+						}
+						if (document.querySelector('#emailRegister').classList.contains('invalid')) {
+							document.querySelector('#emailRegister').classList.remove('invalid');
+						}
 
-					state.validate.email =  true;
-					state.email = v;
-					console.log('📧 Valid - Value: ', state.email);
+						document.querySelector('#emailWarn').style.display = 'none';
+
+						document.querySelector('#emailHelp').style.display = 'none';
+
+
+						state.validate.email =  true;
+						state.email = v;
+						console.log('📧 Valid - Value: ', state.email);
+					} else {
+						if (document.querySelector('#emailRegister').classList.contains('valid')) {
+						document.querySelector('#emailRegister').classList.remove('valid');
+						}
+
+						if (!document.querySelector('#emailRegister').classList.contains('invalid')) {
+							document.querySelector('#emailRegister').classList.add('invalid');
+						}
+						document.querySelector('#emailWarn').style.display = 'block';
+
+						state.validate.email =  false;
+						state.email = null;
+					}
 
 					break;
 				case 'telefono':
@@ -117,6 +137,8 @@ validateField = (c, v) => {
 					if (!document.querySelector('#emailRegister').classList.contains('invalid')) {
 						document.querySelector('#emailRegister').classList.add('invalid');
 					}
+
+					document.querySelector('#emailHelp').style.display = 'block';
 
 					state.validate.email =  false;
 					state.email = null;
@@ -149,12 +171,11 @@ validateField = (c, v) => {
 validatePassword = () => {
 	
 	let psw1 = document.querySelector('#passwordReg1').value;
-	let psw2 = document.querySelector('#passwordReg2').value;
 
 
 	let complex = isOkPass(psw1);
 
-	if (complex.result && psw1 !== '') {
+	if (complex.result && psw1 !== '' && psw1 !== undefined) {
 		if (document.querySelector('#passwordReg1').classList.contains('invalid')) {
 			document.querySelector('#passwordReg1').classList.remove('invalid');
 		}
@@ -162,31 +183,15 @@ validatePassword = () => {
 		if (!document.querySelector('#passwordReg1').classList.contains('valid')) {
 			document.querySelector('#passwordReg1').classList.add('valid');
 		}
-		if (psw1 === psw2 && psw1 !== '') {
-			if (document.querySelector('#passwordReg2').classList.contains('invalid')) {
-				document.querySelector('#passwordReg2').classList.remove('invalid');
-			}
-
-			if (!document.querySelector('#passwordReg2').classList.contains('valid')) {
-				document.querySelector('#passwordReg2').classList.add('valid');
-			}
-			state.password = hex_sha512(psw1);
-			state.validate.password =  true;
-
-			console.log('🔐 Valid - ', [state.password, psw1]);
 
 
-		} else {
-			if (document.querySelector('#passwordReg2').classList.contains('valid')) {
-				document.querySelector('#passwordReg2').classList.remove('valid');
-			}
+		document.querySelector('#psw1Help').style.display = 'none';
 
-			if (!document.querySelector('#passwordReg2').classList.contains('invalid')) {
-				document.querySelector('#passwordReg2').classList.add('invalid');
-			}
-			state.password = null;
-			state.validate.password =  false;
-		}
+		state.password = hex_sha512(psw1);
+
+		state.validate.password =  true;
+
+		console.log('🔐 Valid - ', [state.password, psw1]);
 
 	} else {
 		if (document.querySelector('#passwordReg1').classList.contains('valid')) {
@@ -197,10 +202,17 @@ validatePassword = () => {
 			document.querySelector('#passwordReg1').classList.add('invalid');
 		}
 
+		document.querySelector('#psw1Help').style.display = 'block';
+
+		state.password = null;
+		state.validate.password = false;
+
 		console.log('🔏 Invalid - ', complex.error)
 	}
 
 }
+
+validateInput = (input) => (input.value.length > 8) ? input.classList.add('valid') : input.classList.remove('valid');
 
 function isOkPass(p){
     var anUpperCase = /[A-Z]/;
@@ -239,13 +251,29 @@ function isOkPass(p){
     return obj;
 }
 
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
 
 window.addEventListener('load', function () {
   	
-	document.querySelector('#NomeRegister').addEventListener('change', (e) => state.name = e.target.value);
+	document.querySelector('#NomeRegister').addEventListener('change', (e) => {
+		validateInput(e.target);
+		state.name = e.target.value;
+	});
 	document.querySelector('#emailRegister').addEventListener('change', (e) => validateField('email', e.target.value));
-	document.querySelector('#countryReg').addEventListener('change', (e) => state.prefix = e.target.value);
+	document.querySelector('#countryReg').addEventListener('change', (e) => {
+		validateInput(e.target);
+		state.prefix = e.target.value;
+	});
 	document.querySelector('#telefonoRegister').addEventListener('change', (e) => validateField('telefono', e.target.value));
 	document.querySelectorAll('#passwordReg1, #passwordReg2').forEach((el) => el.addEventListener('change', () => validatePassword()));
 	document.querySelector('#submitBtn').addEventListener('click', (e) => Register(e, state));
